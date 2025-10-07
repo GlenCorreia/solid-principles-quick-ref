d5ff52ad82085cbceb6246855c0ad618a7913f13
Glen Correia <glen_correia@outlook.com>
2025-10-07 23:47:33 +0530

    Update

--- O.rb
+++ O.rb
@@ -2,6 +2,26 @@
 
 # Open for extension, closed for modification
 
+# ❌ Bad
+
+class Greeting
+  def message(type)
+    if type == :formal
+      "Hello"
+    elsif type == :friendly
+      "Hey there!"
+    elsif type == :casual
+      "Yo!"
+    else
+      "Hi"
+    end
+  end
+end
+
+puts Greeting.new.message(:formal)
+puts Greeting.new.message(:friendly)
+puts Greeting.new.message(:casual)
+
 # ✅ Good
 
 class Greeting
