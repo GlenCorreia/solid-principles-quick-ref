bf3c5c3627ec2f5d2337014411d30e5378bc2faa
Glen Correia <glen_correia@outlook.com>
2025-10-07 23:51:14 +0530

    Update

--- I.rb
+++ I.rb
@@ -2,6 +2,38 @@
 
 # Don’t force classes to implement things they don’t need
 
+# ❌ Bad
+
+module Vehicle
+  def drive; end
+  def float; end
+end
+
+class Car
+  include Vehicle
+
+  def drive
+    puts "Driving on road"
+  end
+
+  def float
+    raise "Car can't float!"
+  end
+end
+
+class Boat
+  include Vehicle
+
+  def drive
+    raise "Boat can't drive!"
+  end
+
+  def float
+    puts "Floating on water"
+  end
+end
+
+
 # ✅ Good
 
 module Drivable
