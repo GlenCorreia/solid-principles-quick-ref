03ce26a0181f57c18348aed3e49e61c6e0029dce
Glen Correia <glen_correia@outlook.com>
2025-10-07 23:52:34 +0530

    Update

--- D.rb
+++ D.rb
@@ -2,6 +2,21 @@
 
 # Depend on abstractions, not concrete classes
 
+# ❌ Bad
+
+class Notifier
+  def notify
+    email_sender = EmailSender.new
+    email_sender.send("Hello!")
+  end
+end
+
+class EmailSender
+  def send(message)
+    puts "Email: #{message}"
+  end
+end
+
 # ✅ Good
 
 class EmailSender
